886ddbcc2fbff69b20f9bbaa25b0b12757534455
matthewcmccool-cloud <matthewcmccool@gmail.com>
2026-01-26 15:26:56 -0500

    fix: Disable SWC minification to resolve build errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@clerk/nextjs', '@clerk/shared', '@clerk/backend'],
+      swcMinify: false,
 }
 
 module.exports = nextConfig
