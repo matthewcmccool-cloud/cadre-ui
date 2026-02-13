eb21543376743b090eddda2c828a8aed007a668c
matthewcmccool-cloud <matthewcmccool@gmail.com>
2026-02-13 15:30:30 -0500

    refactor: remove individual dynamic exports, rely on global layout config

--- app/fundraises/page.tsx
+++ app/fundraises/page.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
 };
 
 export const revalidate = 3600;
-export const dynamic = 'force-dynamic';
 
 export default async function FundraisesPage() {
   const [fundraises, filterOptions] = await Promise.all([
